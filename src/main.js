91b20865a62b34889734b85518bffe9f421e3d51
Leonardo Machado <leonardoms@id.uff.br>
2018-11-19 20:58:05 -0200

    Instalado vuetify css e material icons para serem utilizados corrigindo assim o bug do mdi-icon e dos campos de input

--- src/main.js
+++ src/main.js
@@ -1,11 +1,18 @@
+import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
+import 'vuetify/dist/vuetify.min.css' // Ensure you are using css-loader
 import Vue from 'vue'
-import './plugins/vuetify'
+import Vuetify from 'vuetify'
 import App from './App.vue'
 import router from './router'
 
 Vue.config.productionTip = false
 
+Vue.use(Vuetify, {
+  iconfont: 'mdi' // 'md' || 'mdi' || 'fa' || 'fa4'
+})
+
 new Vue({
   router,
   render: h => h(App),
 }).$mount('#app')
+
