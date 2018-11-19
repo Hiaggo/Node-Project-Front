7842f834bd03768bec3997f66daa6fbd4507f374
Leonardo Machado <leonardoms@id.uff.br>
2018-11-19 21:22:19 -0200

    Login setup

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,9 +1,15 @@
 <template>
-    <div class="hello">
-        <h1>{{ msg }}</h1>
+  <v-layout justify-space-around>
+    <v-icon small> mdi_outline_home</v-icon>
+
+    <h1>{{ msg }}</h1>
         <router-link to="/">Log me out!</router-link>
-    </div>
+
+  </v-layout>
+  
 </template>
+
+
 
 <script>
     export default {
@@ -14,24 +20,8 @@
             }
         }
     }
+    
 </script>
 
 <style scoped>
-    h1, h2 {
-        font-weight: normal;
-    }
-
-    ul {
-        list-style-type: none;
-        padding: 0;
-    }
-
-    li {
-        display: inline-block;
-        margin: 0 10px;
-    }
-
-    a {
-        color: #42b983;
-    }
 </style>
