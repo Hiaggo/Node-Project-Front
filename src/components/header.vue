a537427c3e1abd03d443addf784281d1a0ab1a0b
Leonardo Machado <leonardoms@id.uff.br>
2018-12-04 00:40:39 -0200

    lendo nome e papel no header

--- src/components/header.vue
+++ src/components/header.vue
@@ -10,11 +10,13 @@
           </v-card-text>
         </v-card>
       </v-flex>
-  
+
       <v-flex>
-        <v-card dark width="200px" height="200px" class="mr-5" flat tile color="transparent">
+        <v-card dark width="300px" height="200px" class="mr-5" flat tile color="transparent">
           <v-card-text>
-            <v-card-text>Welcome first name IP Address: 127.0.0.1</v-card-text>
+            <v-card-text>Welcome {{ firstName + ' ' + lastName }}<br>Role: {{ role }}</v-card-text>
+
+                   
           </v-card-text>
         </v-card>
       </v-flex>
@@ -23,6 +25,42 @@
 </template>
 
 <script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      firstName: "",
+      lastName: "",
+       role: ""
+    };
+  },
+  created() {
+    const USER_ENDPOINT = "http://localhost:3000/api/user/find";
+    const bearer = "Bearer " + localStorage.getItem("user-token");
+    let self = this;
+
+    axios
+      .post(
+        USER_ENDPOINT,
+        {},
+        {
+          headers: { authorization: bearer }
+        }
+      )
+      .then(function(response) {
+        self.firstName = response.data.data.firstName;
+        self.lastName = response.data.data.lastName;
+        self.role = response.data.data.role;
+      })
+      .catch(function(error) {
+        console.log(error);
+        console.log("error");
+        //console.log("Error on Authentication");
+        //console.log(error);
+      });
+  }
+};
 </script>
 
 <style scoped>
